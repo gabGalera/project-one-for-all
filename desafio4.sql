897fcd9ca012568a4f3807db642ffacbbb99b006
Gabriel Galera <gabgalera@hotmail.com>
2023-01-27 00:27:02 -0300

    Desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,13 +1,23 @@
+WITH AUX(usuario_id, data_reproducao)AS(
+	SELECT
+		usuario_id,
+        MAX(data_reproducao)
+	FROM
+		SpotifyClone.historico_de_reproducoes
+	GROUP BY
+		usuario_id
+)
+
 SELECT 
 	U.usuario,
-    CASE
-		WHEN YEAR(H.data_reproducao) >= 2021 THEN TRUE
-		ELSE FALSE
+  CASE
+		WHEN YEAR(H.data_reproducao) >= 2021 THEN "Usuário ativo"
+		ELSE "Usuário inativo"
 	END AS "status_usuario"
 FROM 
 	SpotifyClone.usuario AS U
 RIGHT JOIN
-	SpotifyClone.historico_de_reproducoes AS H
+	AUX AS H
 ON
 	U.usuario_id=H.usuario_id
 ORDER BY
