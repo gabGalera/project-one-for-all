664818f4f7e74dc7197dd20728fd473311e8c3b1
Gabriel Galera <gabgalera@hotmail.com>
2023-01-27 00:07:16 -0300

    Desafio3

--- desafio3.sql
+++ desafio3.sql
@@ -1,13 +1,26 @@
--- WITH AUX() 
 
-SELECT 
-	U.usuario AS 'usuario',
-    COUNT(H.historico_de_reproducoes) AS "qt_de_musicas_ouvidas"
-FROM 
-	SpotifyClone.historico_de_reproducoes AS H
+WITH AUX(usuario_id, historico_de_reproducoes, duracao_segundos)AS(
+	SELECT
+		H.usuario_id,
+		H.historico_de_reproducoes,
+        C.duracao_segundos
+	FROM
+		SpotifyClone.historico_de_reproducoes AS H
+	LEFT JOIN
+		SpotifyClone.cancoes AS C
+	ON 
+		H.cancoes_id = C.cancoes_id
+)
+
+SELECT
+	MAX(U.usuario) AS 'usuario',
+	COUNT(A.historico_de_reproducoes) AS "qt_de_musicas_ouvidas",
+    COUNT(A.duracao_segundos) AS "total_minutos"
+FROM
+	AUX AS A
 LEFT JOIN
 	SpotifyClone.usuario AS U
 ON
-	U.usuario_id = H.usuario_id
+	U.usuario_id = A.usuario_id
 GROUP BY
 	U.usuario
