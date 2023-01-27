f230ccbfe8497b44ffe0049c8df5ca638521b966
Gabriel Galera <gabgalera@hotmail.com>
2023-01-27 14:55:11 -0300

    Req11

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,26 @@
-SELECT cancoes FROM SpotifyClone.cancoes;
+SELECT 
+	*
+FROM (
+	SELECT 
+		cancoes AS "nome_musica",
+		(REPLACE(
+			REPLACE(
+				REPLACE(
+					REPLACE(
+						REPLACE(cancoes, "SUPERSTAR", "SUPERDEV"), 
+						"SOUL", "CODE"),
+					"Pais", "Pull Requests"), 
+			"Amar", "Code Review"), 
+		"Bard", "QA")
+		 ) AS "novo_nome"
+	FROM 
+		SpotifyClone.cancoes
+	) sub
+WHERE
+	novo_nome like "%QA%" OR
+    novo_nome LIKE "%Code Review%" OR
+    novo_nome LIKE "%Pull Requests%" OR
+    novo_nome LIKE "%CODE%" OR
+    novo_nome LIKE "%SUPERDEV%"
+ORDER BY
+	novo_nome;
