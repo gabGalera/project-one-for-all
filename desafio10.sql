d2e1c2391ef3502072ac1e8dfe7f826bed18ffa5
Gabriel Galera <gabgalera@hotmail.com>
2023-01-27 14:25:10 -0300

    Desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,1 +1,28 @@
-SELECT * FROM SpotifyClone.albuns;
+WITH AUX(usuario_id, plano)AS(
+	SELECT
+		U.usuario_id,
+        P.plano
+	FROM
+		SpotifyClone.usuario AS U
+	LEFT JOIN
+		SpotifyClone.plano AS P
+	ON
+		U.plano_id = P.plano_id
+	WHERE
+		P.plano IN ("gratuito", "pessoal")
+)
+
+SELECT
+    H.historico_de_reproducoes AS "nome",
+	COUNT(AUX.usuario_id) AS "reproducoes"
+FROM
+	AUX
+RIGHT JOIN
+	SpotifyClone.historico_de_reproducoes AS H
+ON
+	AUX.usuario_id = AUX.usuario_id
+GROUP BY
+	nome
+ORDER BY
+	nome
+
