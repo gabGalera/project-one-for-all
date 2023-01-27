95ede8fd7240c1c513b56bdb5c77fb315e287740
Gabriel Galera <gabgalera@hotmail.com>
2023-01-26 23:52:23 -0300

    Cria desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+SELECT 
+	U.usuario AS 'usuario',
+    COUNT(H.historico_de_reproducoes) AS "qt_de_musicas_ouvidas"
+FROM 
+	SpotifyClone.historico_de_reproducoes AS H
+LEFT JOIN
+	SpotifyClone.usuario AS U
+ON
+	U.usuario_id = H.usuario_id
+GROUP BY
+	U.usuario
