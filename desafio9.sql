d0c91d2d186c29c4e636a3bc3edc01af6164f0cf
Gabriel Galera <gabgalera@hotmail.com>
2023-01-27 13:38:39 -0300

    Desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,11 +1,12 @@
 SELECT 
-	COUNT(H.historico_de_reproducoes),
-    U.usuario
+	COUNT(H.historico_de_reproducoes) AS "quantidade_musicas_no_historico"
 FROM 
 	SpotifyClone.historico_de_reproducoes AS H
 LEFT JOIN
 	SpotifyClone.usuario AS U
 ON
 	H.usuario_id = U.usuario_id
+WHERE
+    U.usuario = "Barbara Liskov"
 GROUP BY
 	H.usuario_id
