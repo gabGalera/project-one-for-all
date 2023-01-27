7787b7e4c2e3088c6ebeabf23108ea717eda9889
Gabriel Galera <gabgalera@hotmail.com>
2023-01-27 00:32:48 -0300

    Requisito 5

---
+++ desafio5.sql
@@ -0,0 +1,15 @@
+SELECT 
+	C.cancoes,
+	COUNT(H.usuario_id) AS "reproducoes"
+FROM 
+	SpotifyClone.historico_de_reproducoes AS H
+LEFT JOIN
+	SpotifyClone.cancoes AS C
+ON
+	H.cancoes_id = C.cancoes_id
+GROUP BY
+	C.cancoes
+ORDER BY
+	reproducoes DESC,
+    cancoes
+LIMIT 2
